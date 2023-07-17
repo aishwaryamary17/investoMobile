3aca884db44716938105fd81f358b0db79f8ba22
AishwaryaMaryJ <aishwaryamary17@gmail.com>
2023-07-17 13:30:21 +0400

    sctock graph data taken from the json data

--- Screens/MarketsScreen/StockGraph.tsx
+++ Screens/MarketsScreen/StockGraph.tsx
@@ -11,9 +11,6 @@ type graphProps = {
 
 const StockGraph = (props: graphProps) => {
   const { height, width, color, data } = props;
-  // const data = [
-  //   50, -10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -10,
-  // ];
 
   return (
     <LineChart
@@ -22,7 +19,7 @@ const StockGraph = (props: graphProps) => {
       curve={shape.curveBasis}
       svg={{
         strokeWidth: 2,
-        strokeLinecap: "rounded",
+        strokeLinecap: "round",
         stroke: color,
       }}
     ></LineChart>
