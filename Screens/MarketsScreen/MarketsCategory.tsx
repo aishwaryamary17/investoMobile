64de0b846bda495b2013c28e783e716b4832e3c3
AishwaryaMaryJ <aishwaryamary17@gmail.com>
2023-07-17 13:28:11 +0400

    category screen code optimization and stock card component implement

--- Screens/MarketsScreen/MarketsCategory.tsx
+++ Screens/MarketsScreen/MarketsCategory.tsx
@@ -1,37 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Text, StyleSheet, useWindowDimensions } from "react-native";
-import { TabView, SceneMap, TabBar } from "react-native-tab-view";
+import { TabView, TabBar } from "react-native-tab-view";
 import StockListCard from "./StockListCard";
+import { stockList } from "../../Shared/StockListData";
 
-const renderScene = SceneMap({
-  first: StockListCard,
-  second: StockListCard,
-  third: StockListCard,
-  fourth: StockListCard,
-});
+type ItemData = {
+  id: string;
+  stockName: string;
+  stockSymbol: string;
+  currentPrice: string;
+  percentageGain: number;
+  priceGain: number;
+  stockGraph: {
+    timeRange: string;
+    interval: string;
+    data: number[];
+  };
+};
 
 const MarketsCategory = () => {
   const layout = useWindowDimensions();
   const [activeKey, setActiveKey] = useState("Main Market");
+  const [stockListData, setStockListData] = useState<ItemData[]>(stockList);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [index, setIndex] = useState(0);
   const [routes] = useState([
-    { key: "first", title: "Main Market" },
-    { key: "second", title: "Junior Market" },
-    { key: "third", title: "FX Rates" },
-    { key: "fourth", title: "FX Rate" },
+    { key: "mainMarket", title: "Main Market" },
+    { key: "juniorMarket", title: "Junior Market" },
+    { key: "fxRates", title: "FX Rates" },
+    { key: "funds", title: "Funds" },
   ]);
 
-  const renderTabBar = (props) => {
+  const renderTabBar = (props: any) => {
     return (
       <TabBar
         {...props}
         style={{ backgroundColor: "#2c53f5" }}
         tabStyle={styles.tab}
-        renderLabel={({ route, focused, color }) => (
-          <Text style={focused ? styles.activeLabel : styles.label}>
-            {route.title}
-          </Text>
-        )}
+        renderLabel={({ route, focused, color }) => {
+          return (
+            <Text style={focused ? styles.activeLabel : styles.label}>
+              {route.title}
+            </Text>
+          );
+        }}
         scrollEnabled={true}
         bounces={true}
         initialLayout={{ width: "auto" }}
@@ -40,13 +52,37 @@ const MarketsCategory = () => {
     );
   };
 
+  const renderScene = () => {
+    return (
+      <StockListCard
+        activeKey={activeKey}
+        setIsLoading={setIsLoading}
+        isLoading={isLoading}
+        stockListData={stockListData}
+        setStockListData={setStockListData}
+        isSearch={false}
+      />
+    );
+  };
+
+  const handleIndexChange = (index: number) => {
+    const tabName = routes[index].title;
+    setStockListData([]);
+    setActiveKey(tabName);
+    setIndex(index);
+    setIsLoading(true);
+  };
+
   return (
     <TabView
       navigationState={{ index, routes }}
       renderScene={renderScene}
-      onIndexChange={setIndex}
       renderTabBar={renderTabBar}
+      onIndexChange={handleIndexChange}
       overScrollMode="always"
+      onSwipeStart={() => {
+        setStockListData([]);
+      }}
     />
   );
 };
